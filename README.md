6dbf15289f923057915f1d939e726c8ad2ac082b
Dalton Rooney <dalton@madebyraygun.com>
2013-07-19 16:33:16 -0400

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ acf-chosen
 ==========
 
 Chosen script for Advanced Custom Fields
+
+This applies the chosen.js script to all select fields in the WP admin. You'll probably want to edit the javascript to be much more selective in which select fields it is applied to.
---
+++ acf-chosen.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: Advanced Custom Fields Chosen plugin
+Plugin URI: https://github.com/daltonrooney/acf-chosen
+Description: Adds chosen.js to ACF dropdowns
+Author: Raygun Design, LLC
+Version: 1.0
+
+Author URI: http://madebyraygun.com
+
+Copyright 2013 Raygun Design LLC (email : contact@madebyraygun.com)
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/ 
+
+add_action( 'admin_footer-post.php', 'add_chosen_select' );
+add_action( 'admin_footer-post-new.php', 'add_chosen_select' );
+function add_chosen_select() {?>
+    
+	<script type="text/javascript">
+	jQuery(document).ready( function($) {
+		$(document).live('acf/setup_fields', function(e, div){
+			$("select").chosen();
+    	});
+    });
+	</script>
+<?php }
+
+
+function enqueue_chosen_js() {
+ 	wp_enqueue_script( 'chosen_js', plugins_url( '' ,  __FILE__ ) . '/lib/jquery.chosen.min.js', array('jquery'));
+	wp_enqueue_style( 'chosen_js', plugins_url( '' ,  __FILE__ ) . '/lib/chosen.min.css');
+}
+
+add_action('admin_enqueue_scripts', 'enqueue_chosen_js', 2000);
