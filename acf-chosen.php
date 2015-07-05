31afbb34df093f84452ccc659e548e74b502f784
Aurélien Joahny <aurelien@unikmedia.fr>
2015-07-05 17:41:52 +0200

    Fixes issue and use acf ready action

--- acf-chosen.php
+++ acf-chosen.php
@@ -12,19 +12,22 @@ Copyright 2013 Raygun Design LLC (email : contact@madebyraygun.com)
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-*/ 
+*/
 
-add_action( 'admin_footer-post.php', 'add_chosen_select' );
-add_action( 'admin_footer-post-new.php', 'add_chosen_select' );
-function add_chosen_select() { ?>
-<script type="text/javascript">
-  jQuery(function($) {
-    $(document).on('acf/setup_fields', function() {
-      $('select'. '.acf_postbox').chosen();
-    });
-  });
-</script>
-<?php }
+function acf_chosen_admin_footer() {
+
+  ?>
+  <script type="text/javascript">
+    (function($) {
+      acf.add_action('ready', function( ){
+        $('.acf-input select').chosen();
+      });
+    })(jQuery);
+  </script>
+<?php
+
+}
+add_action('acf/input/admin_footer', 'acf_chosen_admin_footer');
 
 add_action('admin_enqueue_scripts', 'enqueue_chosen_js', 2000 );
 function enqueue_chosen_js() {
